b14994e2587c0cfbc4ea187d16da111906568c65
albator2018 <rene.romain@mail.com>
2018-01-15 03:34:21 +0100

    Weapon

--- game/main.js
+++ game/main.js
@@ -3,16 +3,22 @@ var game = new Phaser.Game(640, 480, Phaser.CANVAS, 'game', { preload: preload, 
 function preload() {
   game.load.tilemap('objects', 'assets/level1-1.json', null, Phaser.Tilemap.TILED_JSON);
   game.load.image('tiles', 'assets/Tileset.png');
-  game.load.spritesheet('ninja', 'assets/Spritesheet.png', 80, 80, 20);
+  //game.load.spritesheet('ninja', 'assets/Spritesheet.png', 140, 80, 25);
+  game.load.atlasJSONHash('ninja', 'assets/Spritesheet.png', 'assets/sprites.json');
   game.load.image("background", "assets/0.png");
+  game.load.image('shuriken', 'assets/shuriken.png');
+  game.load.spritesheet('sprite', 'assets/1.png', 16, 16);
 }
 
 var map;
 var layer1, layer2;
 var cursors;
-var jumpButton;
-var runButton;
 var result;
+var shuriken;
+var enemy;
+var runButton;
+var fireButton;
+var slashButton;
 
 var ninja = {
   sprite: undefined,
@@ -48,22 +54,41 @@ function create() {
   ninja.sprite.anchor.x=0.5;
   ninja.sprite.anchor.y=0.5;
 
+  enemy = game.add.sprite(1000, 405, 'sprite');
+
   game.physics.enable(ninja.sprite);
   game.physics.arcade.gravity.y = 700;
   ninja.sprite.body.bounce.y = 0;
   ninja.sprite.body.linearDamping = 1;
   ninja.sprite.body.collideWorldBounds = true;
 
-  ninja.sprite.animations.add('idle', [0, 1, 2, 3], 10, true);
-  ninja.sprite.animations.add('jump', [5], 10, true);
-  ninja.sprite.animations.add('left', [10, 11, 12], 10, true);
+  //ninja.sprite.animations.add('idle', [0, 1, 2, 3], 10, true);
+  ninja.sprite.animations.add('idle', ['sprite1', 'sprite2', 'sprite3', 'sprite4'], 10, true);
+  //ninja.sprite.animations.add('jump', [5], 10, true);
+  ninja.sprite.animations.add('jump', ['sprite5'], 10, true);
+  //ninja.sprite.animations.add('left', [10, 11, 12], 10, true);
+  ninja.sprite.animations.add('left', ['sprite6', 'sprite7', 'sprite8'], 10, true);
+  //ninja.sprite.animations.add('throw', [15, 16, 17, 18, 19], 10, true);
+  ninja.sprite.animations.add('throw', ['sprite9', 'sprite10', 'sprite11', 'sprite12', 'sprite13'], 10, true);
+  //ninja.sprite.animations.add('slash', [20, 21, 22, 23], 10, true);
+  ninja.sprite.animations.add('slash', ['sprite14', 'sprite15', 'sprite17', 'sprite16'], 10, true);
 
   ninja.sprite.body.fixedRotation = true;
 
   game.camera.follow(ninja.sprite);
+
+  shuriken = game.add.weapon( 1, 'shuriken');
+  shuriken.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
+  shuriken.bulletSpeed = 500;
+
+  shuriken.trackSprite(ninja.sprite, 14, 0, false);
+
+
+
   cursors = game.input.keyboard.createCursorKeys();
-  jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
   runButton = game.input.keyboard.addKey(Phaser.Keyboard.SHIFT);
+  fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+  slashButton = game.input.keyboard.addKey(Phaser.Keyboard.X);
 }
 
 
@@ -112,14 +137,29 @@ function update(){
       }
     }
     ninja.doNothing = false;
-  }
-  if (cursors.up.justDown){
+  } else if (cursors.up.justDown){
     if(ninja.sprite.body.onFloor()){
       ninja.sprite.body.velocity.y = -310;
       ninja.sprite.animations.play('jump', 10, true);
       ninja.doNothing = false;
     }
   }
+
+  if(fireButton.isDown){
+    ninja.sprite.animations.play('throw', 12, true);
+    if (ninja.direction == 'right'){
+      shuriken.fireAngle = Phaser.ANGLE_RIGHT;
+    } else {
+      shuriken.fireAngle = Phaser.ANGLE_LEFT;
+    }
+    shuriken.fire();
+    ninja.doNothing = false;
+  }
+  if(slashButton.isDown){
+    ninja.sprite.animations.play('slash', 10, true);
+    ninja.doNothing = false;
+  }
+
   if(ninja.doNothing){
     if(ninja.sprite.body.velocity.x>10){
       ninja.sprite.body.velocity.x -= 10;
